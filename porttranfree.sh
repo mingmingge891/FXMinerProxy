045845861f4ccb5c0cbb0604690af4fac8c8543d
mingmingge891 <38743557+mingmingge891@users.noreply.github.com>
2022-01-01 23:40:40 +0800

    Update and rename porttran.sh to porttranfree.sh

---
+++ porttranfree.sh
@@ -0,0 +1,17 @@
+#bin
+version='2.2'
+wget https://github.com/mingmingge891/porttran/archive/refs/tags/$version.tar.gz
+tar -zxvf $version.tar.gz
+mv porttran-$version/porttranfree/portdir.sh porttran-$version/porttranfree/porttran;chmod 777 porttran-$version/porttranfree/porttran
+mkdir porttran && chmod 777 porttran
+mv porttran-$version/porttranfree/* porttran1/
+rm -rf porttran-$version
+rm $version.tar.gz
+clear
+echo "======================================="
+echo "download complete,please perform the following steps"
+echo "step:1 cd porttran/"
+echo "step:2 run like this ./porttran port ip:port "
+echo "more help please open readme.txt"
+echo "======================================="
+rm porttranfree.sh
