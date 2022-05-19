4806035122eaa5845163a57514c215576ffdac06
mingmingge891 <38743557+mingmingge891@users.noreply.github.com>
2022-05-20 02:38:09 +0800

    Update porttranpay.sh

--- porttranpay.sh
+++ porttranpay.sh
@@ -1,8 +1,8 @@
 #bin
 version='7.0.1.3.2'
 shell_version='2.3.1'
-uiname='porttran-ui'
-pkgname='porttran'
+uiname='FXMinerProxy-shell'
+pkgname='FXMinerProxy'
 authorname='mingmingge891'
 installname='porttranpay.sh'
 webuiname='ui'
